c19bd9ec3175bfe20694edfb9aa2897dd058f2f4
Erick <erick.retana@pfcti.com>
2019-08-12 12:29:14 -0600

    Cambio en los nombres de las JPanel Form

--- BuyGames/src/buygames/BuyGames.java
+++ BuyGames/src/buygames/BuyGames.java
@@ -8,7 +8,7 @@ public class BuyGames {
     public static void main(String[] args) {
         
         //Llamado a la primer ventana de Login
-        login frame = new login();
+        VentanaLogin frame = new VentanaLogin();
         frame.setSize(800,650);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
