99836da63afb9f362f1b14cea90f54064f408754
Erick <erick.retana@pfcti.com>
2019-08-15 10:33:46 -0600

    Se agregan variables y se crea clase empleado

---
+++ BuyGames/src/buygames/VideoJuegos.java
@@ -0,0 +1,13 @@
+package buygames;
+/**
+ *
+ * @author erick.retana
+ */
+public class VideoJuegos {
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private String cedula;
+}
